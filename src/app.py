8817b5e9becd88f5d62f8113d2eee2f1dcbdf2ba
DE4Fxx <65514584+DE4Fxx@users.noreply.github.com>
2026-03-13 22:26:55 -0400

    Revert "Feature/cli support"

--- src/app.py
+++
@@ -1,9 +0,0 @@
-from cli import run_cli_app
-from modules import JsonFilePersistence
-
-def main():
-    storage = JsonFilePersistence("orders.json")  # will persist to orders.json
-    run_cli_app(storage)
-
-if __name__ == "__main__":
-    main()
